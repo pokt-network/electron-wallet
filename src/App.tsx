5b22d8cfc6b2c2aa4beb2a6b4dc4b29718d2fd3a
rBurgett <ryan@burgettdev.net>
2021-10-29 01:12:06 -0400

    Added react, typescript, and sass

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      <h1 className={'text-center'}>dl-wallet</h1>
+    </div>
+  );
+}
+
+export default App;
