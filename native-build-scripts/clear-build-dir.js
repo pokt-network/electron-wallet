b557bc82599eccd933d85dc42e8654a32d7e10d7
rBurgett <ryan@burgettdev.net>
2021-11-12 13:41:32 -0500

    Added mac build Dockerfile

--- native-build-scripts/clear-build-dir.js
+++ native-build-scripts/clear-build-dir.js
@@ -1,12 +1,29 @@
+const fs = require('fs-extra');
 const path = require('path');
 const rmrf = require('rmrf-promise');
 
-const buildDir = path.resolve(__dirname, '../build');
-
 (async function() {
 
-  console.log('Clearing build directory\n');
+  const clearDirectory = async function(dir) {
+    console.log(`Clearing ${dir}\n`);
 
-  await rmrf(buildDir);
+    await fs.ensureDir(dir);
+
+    const contents = await fs.readdir(dir);
+
+    for(const file of contents) {
+      const filePath = path.join(dir, file);
+      await rmrf(filePath);
+    }
+
+  }
+
+  const dirs = [
+    'build',
+    'build-native',
+  ];
+  for(const dir of dirs) {
+    await clearDirectory(path.resolve(__dirname, `../${dir}`));
+  }
 
 })();
