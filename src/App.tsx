b61c3c1e98120e5c1001b3832b7f0f96f1a0bee6
rBurgett <ryan@burgettdev.net>
2021-12-10 13:07:48 -0500

    Added pocket ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { WalletController } from './modules/wallet-controller';
+import { textStyle } from './styles';
 
 interface AppProps {
   version: string;
@@ -8,7 +9,10 @@ interface AppProps {
 function App({ version, walletController }: AppProps) {
   return (
     <div>
-      <h1 className={'text-center'}>Wallet v{version}</h1>
+      <h1 className={'text-center'} style={textStyle('title1')}>Wallet v{version}</h1>
+      <h2 className={'text-center'} style={textStyle('title2')}>Wallet v{version}</h2>
+      <h3 className={'text-center'} style={textStyle('title3')}>Wallet v{version}</h3>
+      <h4 className={'text-center'} style={textStyle('title4')}>Wallet v{version}</h4>
     </div>
   );
 }
