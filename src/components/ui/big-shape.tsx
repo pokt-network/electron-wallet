0f8ded3b12497c04963113d96f1dec79f71fb628
rBurgett <ryan@burgettdev.net>
2022-03-30 03:51:12 -0400

    Added big shapes to main views

--- src/components/ui/big-shape.tsx
+++ src/components/ui/big-shape.tsx
@@ -1,6 +1,9 @@
 import circle from '../../images/big-shapes/circle.svg';
+import circleLg from '../../images/big-shapes/circle-lg.svg';
 import square from '../../images/big-shapes/square.svg';
+import squareLg from '../../images/big-shapes/square-lg.svg';
 import triangle from '../../images/big-shapes/triangle.svg';
+import triangleLg from '../../images/big-shapes/triangle-lg.svg';
 
 export type BigShapeType = "circle"|"square"|"triangle"
 
@@ -11,9 +14,9 @@ interface BigShapeProps {
 }
 export const BigShape = ({ name, size = 'md', style = {}}: BigShapeProps) => {
   const sources = {
-    circle,
-    square,
-    triangle
+    circle: size === 'lg' ? circleLg : circle,
+    square: size === 'lg' ? squareLg : square,
+    triangle: size === 'lg' ? triangleLg : triangle,
   };
   return (
     <img alt={name} style={style} src={sources[name]} />
