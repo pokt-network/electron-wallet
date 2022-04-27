520fc25343d9b6fd70d1f0862a6020e65a4a5230
rBurgett <ryan@burgettdev.net>
2022-04-27 03:23:22 -0400

    Centered confirm delete address modal

--- src/components/views/address-book.tsx
+++ src/components/views/address-book.tsx
@@ -180,12 +180,12 @@ export const AddressBook = () => {
             </div>
           </FlexColumn>
         </MainBody>
-        {showConfirmDeleteModal ?
-          <ModalConfirm title={localize.text('Delete Address', 'addressBook')} text={localize.text('Are you sure you want to delete {{name}}-{{address}} from the address book?', 'addressBook', {name: selectedAddress?.name, address: selectedAddress?.address.slice(-4)})} shape={'circle'} confirmButtonText={localize.text('Delete', 'addressBook')} onCancel={onDeleteModalCancel} onConfirm={onDeleteModalConfirm} />
-          :
-          null
-        }
       </MainContainer>
+      {showConfirmDeleteModal ?
+        <ModalConfirm title={localize.text('Delete Address', 'addressBook')} text={localize.text('Are you sure you want to delete {{name}}-{{address}} from the address book?', 'addressBook', {name: selectedAddress?.name, address: selectedAddress?.address.slice(-4)})} shape={'circle'} confirmButtonText={localize.text('Delete', 'addressBook')} onCancel={onDeleteModalCancel} onConfirm={onDeleteModalConfirm} />
+        :
+        null
+      }
     </FlexRow>
   );
 }
